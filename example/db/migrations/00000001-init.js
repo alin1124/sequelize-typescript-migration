f3eb3c481e707a9857a6bf4459d9ec534fd82c02
Qin Hong <qin@haventech.us>
2024-04-12 16:15:50 -0400

    added capacity to generate custom migration template

--- example/db/migrations/00000001-init.js
+++ example/db/migrations/00000001-init.js
@@ -1,207 +1,138 @@
 'use strict';
 
-const Sequelize = require('sequelize');
-
-/**
- * Actions summary:
- *
- * createTable "CarBrands", deps: []
- * createTable "Cars", deps: [CarBrands]
- *
- **/
-
-const info = {
-    "revision": 1,
-    "name": "init",
-    "created": "2022-09-29T10:45:59.104Z",
-    "comment": ""
-};
-
-const migrationCommands = [
-
-    {
-        fn: "createTable",
-        params: [
-            "SequelizeMigrationsMeta",
-            {
-                "revision": {
-                    "primaryKey": true,
-                    "type": Sequelize.INTEGER
-                },
-                "name": {
-                    "allowNull": false,
-                    "type": Sequelize.STRING
-                },
-                "state": {
-                    "allowNull": false,
-                    "type": Sequelize.JSON
-                },
-            },
-            {}
-        ]
-    },
-    {
-        fn: "bulkDelete",
-        params: [
-            "SequelizeMigrationsMeta",
-            [{
-                revision: info.revision
-            }],
-            {}
-        ]
-    },
-    {
-        fn: "bulkInsert",
-        params: [
-            "SequelizeMigrationsMeta",
-            [{
-                revision: info.revision,
-                name: info.name,
-                state: '{"revision":1,"tables":{"CarBrands":{"tableName":"CarBrands","schema":{"id":{"seqType":"Sequelize.INTEGER","allowNull":false,"primaryKey":true,"autoIncrement":true},"name":{"seqType":"Sequelize.STRING"},"isCertified":{"seqType":"Sequelize.BOOLEAN"},"imgUrl":{"seqType":"Sequelize.STRING"},"orderNo":{"seqType":"Sequelize.INTEGER"},"carsCount":{"seqType":"Sequelize.INTEGER"},"createdAt":{"seqType":"Sequelize.DATE","allowNull":false},"updatedAt":{"seqType":"Sequelize.DATE","allowNull":false}},"indexes":{}},"Cars":{"tableName":"Cars","schema":{"id":{"seqType":"Sequelize.INTEGER","allowNull":false,"primaryKey":true,"autoIncrement":true},"name":{"seqType":"Sequelize.STRING"},"carBrandId":{"seqType":"Sequelize.INTEGER","allowNull":true,"references":{"model":"CarBrands","key":"id"},"onUpdate":"CASCADE","onDelete":"NO ACTION"},"createdAt":{"seqType":"Sequelize.DATE","allowNull":false},"updatedAt":{"seqType":"Sequelize.DATE","allowNull":false}},"indexes":{}}}}'
-            }],
-            {}
-        ]
-    },
-
-
-
-
-    {
-        fn: "createTable",
-        params: [
-            "CarBrands",
-            {
-                "id": {
-                    "autoIncrement": true,
-                    "primaryKey": true,
-                    "allowNull": false,
-                    "type": Sequelize.INTEGER
-                },
-                "name": {
-                    "type": Sequelize.STRING
-                },
-                "isCertified": {
-                    "type": Sequelize.BOOLEAN
-                },
-                "imgUrl": {
-                    "type": Sequelize.STRING
-                },
-                "orderNo": {
-                    "type": Sequelize.INTEGER
-                },
-                "carsCount": {
-                    "type": Sequelize.INTEGER
-                },
-                "createdAt": {
-                    "allowNull": false,
-                    "type": Sequelize.DATE
-                },
-                "updatedAt": {
-                    "allowNull": false,
-                    "type": Sequelize.DATE
-                }
-            },
-            {}
-        ]
-    },
-
-    {
-        fn: "createTable",
-        params: [
-            "Cars",
-            {
-                "id": {
-                    "autoIncrement": true,
-                    "primaryKey": true,
-                    "allowNull": false,
-                    "type": Sequelize.INTEGER
-                },
-                "name": {
-                    "type": Sequelize.STRING
-                },
-                "carBrandId": {
-                    "onDelete": "NO ACTION",
-                    "onUpdate": "CASCADE",
-                    "references": {
-                        "model": "CarBrands",
-                        "key": "id"
-                    },
-                    "allowNull": true,
-                    "type": Sequelize.INTEGER
-                },
-                "createdAt": {
-                    "allowNull": false,
-                    "type": Sequelize.DATE
-                },
-                "updatedAt": {
-                    "allowNull": false,
-                    "type": Sequelize.DATE
-                }
-            },
-            {}
-        ]
-    }
-];
-
-const rollbackCommands = [
-
-    {
-        fn: "bulkDelete",
-        params: [
-            "SequelizeMigrationsMeta",
-            [{
-                revision: info.revision,
-            }],
-            {}
-        ]
-    },
-
-
-
-    {
-        fn: "dropTable",
-        params: ["Cars"]
-    },
-    {
-        fn: "dropTable",
-        params: ["CarBrands"]
-    }
-];
-
-module.exports = {
-  pos: 0,
-  up: function(queryInterface, Sequelize) {
-    let index = this.pos;
-
-    return new Promise(function(resolve, reject) {
-      function next() {
-        if (index < migrationCommands.length) {
+  const Sequelize = require('sequelize');
+  
+  /**
+   * Actions summary:
+   *
+   * Initialize Migration Metadata 
+   *
+   **/
+  
+  const info = {
+      "revision": 1,
+      "name": "tableSync",
+      "created": "2024-04-11T19:34:11.031Z",
+      "comment": ""
+  };
+  
+  const migrationCommands = [
+      {
+          fn: "dropTable",
+          params: ["SequelizeMeta"]
+      },
+      {
+          fn: "createTable",
+          params: [
+              "SequelizeMeta",
+              {
+                  "name": {
+                      "allowNull": false,
+                      "primaryKey": true,
+                      "type": Sequelize.STRING
+                  },
+                  "createdAt": {
+                      "allowNull": false,
+                      "type": Sequelize.DATE,
+                      "defaultValue": Sequelize.fn('now')
+                  },
+                  "updatedAt": {
+                      "allowNull": false,
+                      "type": Sequelize.DATE,
+                      "defaultValue": Sequelize.fn('now')
+                  }
+              },
+              {}
+          ]
+      },
+  
+      {
+          fn: "createTable",
+          params: [
+              "SequelizeMigrationsMeta",
+              {
+                  "revision": {
+                      "primaryKey": true,
+                      "type": Sequelize.INTEGER
+                  },
+                  "name": {
+                      "allowNull": false,
+                      "type": Sequelize.STRING
+                  },
+                  "state": {
+                      "allowNull": false,
+                      "type": Sequelize.JSON
+                  },
+                  "createdAt": {
+                      "allowNull": false,
+                      "type": Sequelize.DATE,
+                      "defaultValue": Sequelize.fn('now')
+                  },
+                  "updatedAt": {
+                      "allowNull": false,
+                      "type": Sequelize.DATE,
+                      "defaultValue": Sequelize.fn('now')
+                  }
+              },
+              {}
+          ]
+      },
+      {
+          fn: "bulkDelete",
+          params: [
+              "SequelizeMigrationsMeta",
+              [{
+                  revision: info.revision
+              }],
+              {}
+          ]
+      },
+      {
+          fn: "bulkInsert",
+          params: [
+              "SequelizeMigrationsMeta",
+              [{
+                  revision: info.revision,
+                  name: info.name,
+                  state: '{"revision":1,"tables":{}}'
+              }],
+              {}
+          ]
+      },
+  ];
+  
+  const rollbackCommands = [
+  
+      {
+          fn: "bulkDelete",
+          params: [
+              "SequelizeMigrationsMeta",
+              [{
+                  revision: info.revision,
+              }],
+              {}
+          ]
+      },
+  ];
+  
+  module.exports = {
+    async up (queryInterface, Sequelize) {
+      let index = 0;
+      while (index < migrationCommands.length) {
           let command = migrationCommands[index];
           console.log("[#"+index+"] execute: " + command.fn);
           index++;
-          queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
-        } else resolve();
+          await queryInterface[command.fn].apply(queryInterface, command.params);
       }
-
-      next();
-    });
-  },
-  down: function(queryInterface, Sequelize) {
-    let index = this.pos;
-
-    return new Promise(function(resolve, reject) {
-      function next() {
-        if (index < rollbackCommands.length) {
+    },
+    async down (queryInterface, Sequelize) {
+      let index = 0;
+      while (index < rollbackCommands.length) {
           let command = rollbackCommands[index];
           console.log("[#"+index+"] execute: " + command.fn);
           index++;
-          queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
-        }
-        else resolve();
+          await queryInterface[command.fn].apply(queryInterface, command.params);
       }
-
-      next();
-    });
-  },
-  info
-};
+    },
+    info
+  };   
